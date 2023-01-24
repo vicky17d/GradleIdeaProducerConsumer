526a90d7120a740c7efccbc6846406f3bb504289
Vicky Dahuja <vdahuja@vdahuja-mn4.linkedin.biz>
2023-01-23 17:45:00 -0800

    Initial Commit

---
+++ consumer/src/main/java/org/example/apps/Main.java
@@ -0,0 +1,10 @@
+package org.example.apps;
+
+import org.example.MainApi;
+import org.example.FooApi;  //TODO Issue #1: This compiles in IntelliJ but not in Gradle
+
+
+public class Main {
+    MainApi mainApi;
+    FooApi fooApi;
+}
